3f4697c4b2a4b3af11a967efb9001d966d636bed
Francis <francisdiasbr@gmail.com>
2022-02-25 17:20:30 -0300

    refact: Refatora o componente

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './BusinessList.css';
+
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList" >
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-      </div >
-    )
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} />
+          })
+        }
+      </div>
+    );
   }
 }
 
-export default BusinessList
+export default BusinessList;
