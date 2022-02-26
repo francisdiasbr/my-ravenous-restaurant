7ba4ac9d8a746f7100af86a1678fe9459a150282
Francis <francisdiasbr@gmail.com>
2022-02-26 16:32:45 -0300

    feat: Adiciona funcionalidade ao SearchBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,16 +5,16 @@ import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
 const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
-  city: 'Flavortown',
+  city: 'Bordertown',
   state: 'NY',
   zipCode: '10101',
   category: 'Italian',
   rating: 4.5,
   reviewCount: 90
-}
+};
 
 const businesses = [
   business,
@@ -24,12 +24,17 @@ const businesses = [
   business,
   business
 ];
+
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses} />
       </div>
     );
